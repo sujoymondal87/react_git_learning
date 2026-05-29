c0f89b196366ad259217b5f7b6e174c5784660e4
Sujoy Kumar Mondal <sujoymondal87@gmail.com>
2026-05-29 19:40:09 +0530

    v2 - resend email confirmation added

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,5 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
+
+const { Resend } = require('resend');
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
@@ -7,6 +10,9 @@ const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
 );
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 router.post('/', async (req, res) => {
     const { fullName, email, phone, message } = req.body;
   
@@ -19,6 +25,14 @@ router.post('/', async (req, res) => {
     if (error) {
       return res.status(500).json({ error: error.message });
     }
+
+    await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: email,
+        subject: 'Thanks for reaching out',
+        html: '<p>Thank you for reaching out to us. We will get back to you as soon as possible.</p>'
+      });
+
     // return success response
     return res.status(200).json({ message: 'Contact added successfully' });
 });
